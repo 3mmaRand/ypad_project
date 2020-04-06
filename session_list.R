19c4b5c07fc505a8b889493389128f1e5fb092fa
Emma Rand <emma.rand@york.ac.uk>
2020-04-06 16:01:07 +0100

    added sessions filtered to required only

--- session_list.R
+++ session_list.R
@@ -4,21 +4,26 @@ library(janitor)
 file <- "data/timetable_bio_stage1.xlsx"
 sessions <- read_excel(file) %>% clean_names()
 # 394 sessions
+# Tidy: many sessions are repeated
+# therefore separate the activity reference from the repeat number, 
+# then filter so we have just the first incidence, the non-17C modules
+# and the non-optional sesions
 sessions <- sessions %>% 
   separate(activity_reference, 
            into = c("activity", "rep"),
-           sep = "/")
-sessions <- sessions %>% 
-  separate(duration, 
-           into = c("hours", "mins"),
-           sep = ":")
-sessions$hours <- as.numeric(sessions$hours)
+           sep = "/") %>% 
+  filter(rep == "01") %>% 
+  filter(module_code != "BIO00017C-A") %>%
+  filter(type != "Optional Activity") %>%
+  filter(type != "Presentation")
+# this results in 167 sessions
 
+# Number of sessions in each module
 
-sessions <- sessions %>% 
-  filter(rep == "01")
-# 239 sessions
-# 
+sessionssummary <- sessions %>% 
+  group_by(module_code, type) %>% 
+  summarise(n = length(type))
 
-nums <- str_count(sessions$staff_member_s, pattern = fixed("("))
-hist(nums)
+# write to file
+file <- "data/sessions.xlsx"
+write_excel_csv(sessions, path = file)
